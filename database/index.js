a6e9785fc19d95d777cb045019bbb38127707ae1
PieroArbildo <89119204+PieroArbildo@users.noreply.github.com>
2025-04-17 14:01:09 -0500

    week 5

--- database/index.js
+++ database/index.js
@@ -7,6 +7,7 @@ const { Pool } = require("pg")
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
+/*
 let pool
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
@@ -35,6 +36,37 @@ module.exports = {
     connectionString: process.env.DATABASE_URL,
   })
   module.exports = pool
+}*/
+
+
+
+let pool
+if (process.env.NODE_ENV == "development") {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  })
+
+  module.exports = {
+    async query(text, params) {
+      try {
+        const res = await pool.query(text, params)
+        console.log("executed query", { text })
+        return res
+      } catch (error) {
+        console.error("error in query", { text })
+        throw error
+      }
+    },
+  }
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  })
+  module.exports = pool
 }
-
-
